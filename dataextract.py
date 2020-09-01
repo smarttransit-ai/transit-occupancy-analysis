ee62d5d28e0030daead1aac159ac3361f0a77f63
abhishek <8484451+adubey14@users.noreply.github.com>
2020-09-01 10:49:51 -0500

    redid transit dashbaord

--- dataextract.py
+++ dataextract.py
@@ -8,3 +8,8 @@ def decompress_pickle(file):
     data = bz2.BZ2File(file, 'rb')
     data = pickle.load(data)
     return data
+
+# Pickle a file and then compress it into a file with extension
+def compress_pickle(title, data):
+    with bz2.BZ2File(f'{title}.pbz2', 'w') as f:
+        pickle.dump(data, f)
